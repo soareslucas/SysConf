aebd2f11e6a62dc3edc77cb4c267946c9a05e2fa
lsrodrigues <lsrodrigues@10.8.13.171>
2018-03-13 09:49:48 -0300

    Atualização de BD.

--- confeccao.sql
+++ confeccao.sql
@@ -1,20 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.1.12
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: 17-Jun-2015 às 00:25
--- Versão do servidor: 5.6.16
--- PHP Version: 5.5.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `confeccao`
